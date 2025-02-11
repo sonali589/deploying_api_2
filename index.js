4502082b2da7d8c076d566edb3db26c010a95885
PV SONALI <pv.sonali.official@gmail.com>
2025-02-11 11:58:57 +0530

    added files

--- index.js
+++ index.js
@@ -4,18 +4,9 @@ const { resolve } = require('path');
 const app = express();
 const port = 3010;
 
-app.use(express.static('static'));
-
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
-
 require('dotenv').config();
+
+
 
 const isAdmin = process.env.IS_ADMIN === 'true';
 
@@ -24,3 +15,15 @@ if (isAdmin) {
 } else {
   console.log("Access restricted. Admin only.");
 }
+
+
+
+app.get('/', (req, res) => {
+  if (process.env.IS_ADMIN === 'true') {
+    res.send({ message: "Welcome, Admin!", data: ["Admin Data 1", "Admin Data 2"] });
+  } else {
+    res.send({ message: "Welcome, User!", data: ["User Data 1", "User Data 2"] });
+  }
+});
+
+app.listen(3000, () => console.log("Server is running on port 3000."));
